bc393f3b8d561c79ac9a653e9d07ea8a9543480b
Shayne <shayne.fawcett@gmail.com>
2023-01-24 16:50:46 -0500

    added functionality for share button and setup comm showcase page to display cards

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,3 +1,6 @@
+
+import FileSaver from 'file-saver'
+
 import { surpriseMePrompts } from '../constants'
 
 export function getRandomPrompt(prompt) {
@@ -8,4 +11,8 @@ export function getRandomPrompt(prompt) {
  
    return randomPrompt
  }
+
+ export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`)
+}
 
