55a29ad7e012ca9bf083778b260fdf22fcd4cab0
Shayne <shayne.fawcett@gmail.com>
2023-01-27 11:13:51 -0500

    resolving cors error

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ dotenv.config()
 
 const app = express()
 app.use(cors({
-   origin: '*'
+   origin: 'https://dall-e-generator.vercel.app/',
+   credentials: true
 }))
 app.use(express.json({ limit: '50mb' }))
 
